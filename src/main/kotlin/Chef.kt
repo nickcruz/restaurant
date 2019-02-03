2b17a1ea25a0e4cc9263b5c87ecce57b7a8aa800
Nick Cruz <ncruz@umich.edu>
2019-02-03 11:42:39 -0800

    Fix unit tests for Chef.
    Unit test is set up to close resources at the end of the test.
    
    This still doesn't feel as intuitive as it can--it's implementing
    Closeable, which, while it's a powerful design pattern, is still a Java
    interface. Since we're trying to make this as far Kotlin in one
    direction as we can, I want to try to find a more idiomatically Kotlin
    way to clean up after the Chefs.
    
    Perhaps the restaurant itself manages the jobs. Or a literal
    RestaurantManager job that closes other jobs (but who closes their
    job?).
    
    In any case, it seems like a Chef should implement a Job, so it's much
    easier to imagine how things interact with each other, as it enforces
    the role-playing and the metaphor.

--- src/main/kotlin/Chef.kt
+++ src/main/kotlin/Chef.kt
@@ -1,22 +1,40 @@
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.channels.SendChannel
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.io.Closeable
 
-class Chef(private val kitchenCounter: KitchenCounter) {
+/**
+ * Cooks food after a short time.
+ */
+class Chef(private val orders: ReceiveChannel<Order>, private val meals: SendChannel<Meal>) : Closeable {
 
-    suspend fun cookOrders() {
-        for (order in kitchenCounter) {
-            print("Cooking $order...")
-            delay(COOK_DELAY)
-            print("Done.\n")
-            kitchenCounter.send(Meal(order.dish, order.customer))
+    private var job: Job? = null
+
+    // TODO: Maybe it'd even be easier to imagine if Chef implemented the job, so that we have a 1:1 object:coroutine ratio.
+    fun startCooking() {
+        close()
+        job = GlobalScope.launch {
+            for (order in orders) {
+                meals.send(cookOrder(order))
+            }
         }
+    }
+
+    override fun close() {
+        job?.cancel()
+    }
+
+    private suspend fun cookOrder(order: Order): Meal {
+        print("Cooking $order...")
+        delay(COOK_DELAY)
+        print("Done.\n")
+        return Meal(order.dish, order.customer)
     }
 
     companion object {
         const val COOK_DELAY = 2000L
     }
 }
-
-class KitchenCounter(private val orders: ReceiveChannel<Order>, private val meals: SendChannel<Meal>) :
-    ReceiveChannel<Order> by orders, SendChannel<Meal> by meals
--- src/test/kotlin/ChefTest.kt
+++ src/test/kotlin/ChefTest.kt
@@ -1,22 +1,95 @@
 import io.kotlintest.TestCase
+import io.kotlintest.TestResult
+import io.kotlintest.data.forall
+import io.kotlintest.matchers.numerics.shouldBeGreaterThan
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
+import io.kotlintest.tables.row
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.Channel.Factory.UNLIMITED
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeout
+import kotlin.system.measureTimeMillis
 
 class ChefTest : StringSpec() {
 
-    lateinit var word: String
+    private val testTimeout = 5000L
+
+    private val customer = Customer()
+
+    private val spaghettiDish = Dish("Spaghetti", 10)
+    private val spaghettiOrder = Order(spaghettiDish, customer)
+    private val spaghettiMeal = Meal(spaghettiDish, customer)
+
+    private val adoboDish = Dish("Adobo", 8)
+    private val adoboOrder = Order(adoboDish, customer)
+    private val adoboMeal = Meal(adoboDish, customer)
+
+    private val chefsCount = 2
+
+    private lateinit var orders: Channel<Order>
+    private lateinit var meals: Channel<Meal>
+
+    private lateinit var chefs: List<Chef>
 
     override fun beforeTest(testCase: TestCase) {
-        word = "Hello"
+        orders = Channel()
+        meals = Channel(UNLIMITED) // Assume infinite waiters. TODO: Add unit tests for having less waiters.
+
+        chefs = List(chefsCount) { autoClose(Chef(orders, meals)) }
+    }
+
+    override fun afterTest(testCase: TestCase, result: TestResult) {
+        orders.close()
+        meals.close()
+
+        closeResources()
     }
 
     init {
-        "word length 5" {
-            word.length shouldBe 5
+        "sending order cooks meal" {
+            forall(
+                row(spaghettiOrder, spaghettiMeal),
+                row(adoboOrder, adoboMeal)
+            ) { order, meal ->
+                chefs.map { it.startCooking() }
+                runBlocking {
+                    withTimeout(testTimeout) {
+                        orders.send(order)
+                        withContext(Dispatchers.Default) { meals.receive() } shouldBe meal
+                    }
+                }
+            }
         }
 
-        "word is \"Hello\"" {
-            word shouldBe "Hello"
+        "two cooks cook faster than one" {
+            val cookTwoMeals = suspend {
+                orders.send(spaghettiOrder)
+                orders.send(adoboOrder)
+                meals.receive()
+                meals.receive()
+            }
+            chefs[0].startCooking()
+            val oneCook = measureTimeMillis {
+                runBlocking {
+                    withTimeout(testTimeout) {
+                        cookTwoMeals()
+                    }
+                }
+            }
+
+            chefs[1].startCooking()
+            val twoCooks = measureTimeMillis {
+                runBlocking {
+                    withTimeout(testTimeout) {
+                        cookTwoMeals()
+                    }
+                }
+            }
+
+            oneCook shouldBeGreaterThan twoCooks
         }
     }
 }
