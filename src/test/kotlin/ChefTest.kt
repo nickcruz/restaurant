99a4b792a21ca0200d88c8324e9be3c9fb80dd65
Nick Cruz <ncruz@umich.edu>
2019-02-09 11:22:43 -0800

    Test delay comparisons for different-speed Chefs.
    In order to ensure the tests are as robust as possible, there is no
    assertion on the exact timings of the tests. But in order to test their
    assertions, we launch coroutines, time them, and compare their times.
    
    More tests may be added as functionality changes.

--- src/test/kotlin/ChefTest.kt
+++ src/test/kotlin/ChefTest.kt
@@ -1,20 +1,20 @@
 import io.kotlintest.TestCase
+import io.kotlintest.matchers.beLessThan
+import io.kotlintest.should
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlin.system.measureTimeMillis
 
 class ChefTest : StringSpec() {
-
-    private val testTimeout = 5000L
 
     private val customer = Customer()
 
     private val spaghettiDish = Dish("Spaghetti", 10)
     private val spaghettiOrder = Order(spaghettiDish, customer)
     private val spaghettiMeal = Meal(spaghettiDish, customer)
-
-    private val adoboDish = Dish("Adobo", 8)
-    private val adoboOrder = Order(adoboDish, customer)
-    private val adoboMeal = Meal(adoboDish, customer)
 
     private lateinit var nick: Chef
     private lateinit var emily: Chef
@@ -28,6 +28,41 @@ class ChefTest : StringSpec() {
         "name" {
             nick.name shouldBe "Nick"
             emily.name shouldBe "Emily"
+        }
+
+        "spaghetti order cooks as a spaghetti meal" {
+            nick.cookOrder(spaghettiOrder) shouldBe spaghettiMeal
+            emily.cookOrder(spaghettiOrder) shouldBe spaghettiMeal
+        }
+
+        "Emily cooks faster than Nick" {
+            val nickCookAsync = async { measureTimeMillis { runBlocking { nick.cookOrder(spaghettiOrder) } } }
+            val emilyCookAsync = async { measureTimeMillis { runBlocking { emily.cookOrder(spaghettiOrder) } } }
+
+            val nickCookTime = nickCookAsync.await()
+            val emilyCookTime = emilyCookAsync.await()
+
+            emilyCookTime should beLessThan(nickCookTime)
+        }
+
+        "Nick and Emily cooking in parallel cooks faster than sequentially" {
+            val parallelCooking = async {
+                measureTimeMillis {
+                    listOf(launch { nick.cookOrder(spaghettiOrder) }, launch { emily.cookOrder(spaghettiOrder) })
+                        .forEach { it.join() }
+                }
+            }
+            val sequentialCooking = async {
+                measureTimeMillis {
+                    launch { nick.cookOrder(spaghettiOrder) }.join()
+                    launch { emily.cookOrder(spaghettiOrder) }.join()
+                }
+            }
+
+            val parallelCookingTime = parallelCooking.await()
+            val sequentialCookingTime = sequentialCooking.await()
+
+            parallelCookingTime should beLessThan(sequentialCookingTime)
         }
     }
 }
